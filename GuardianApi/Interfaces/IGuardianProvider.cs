c109e6319da408fcfb6385041e4a524b7aff44af
Eric Magers <emagers3@gatech.edu>
2018-04-22 10:16:53 -0400

    Adds string response methods to interface

--- GuardianApi/Interfaces/IGuardianProvider.cs
+++ GuardianApi/Interfaces/IGuardianProvider.cs
@@ -4,12 +4,17 @@ using System.Threading.Tasks;
 
 namespace GuardianApi.Interfaces
 {
-    interface IGuardianProvider
+    public interface IGuardianProvider
     {
         Task<ContentSearchResponse> GetContent(string queryString, ContentFilter filters, Pagination pagination);
+        Task<string> GetContent(string queryString, ContentFilter contentFilter, Pagination pagination, string format = "json");
         Task<ContentResponse> GetContent(string id, ContentFilter filters);
+        Task<string> GetContent(string id, ContentFilter filters, string format = "json");
         Task<EditionResponse> GetEditions(string queryString);
+        Task<string> GetEditions(string queryString, string format = "json");
         Task<TagResponse> GetTags(string queryString, TagFilter filters, Pagination pagination);
+        Task<string> GetTags(string queryString, TagFilter filters, Pagination pagination, string format = "json");
         Task<SectionResponse> GetSections(string queryString);
+        Task<string> GetSections(string queryString, string format = "json");
     }
 }
